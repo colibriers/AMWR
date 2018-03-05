f64e0452b91f166c5624fe66674f89b2670d42e2
shevchenco123 <1007380426@qq.com>
2018-03-05 13:13:22 +0800

    modify format and delelt unused info in colibri_aiv_node.cpp

--- colibri_aiv/src/colibri_aiv_node.cpp
+++ colibri_aiv/src/colibri_aiv_node.cpp
@@ -1,37 +1,25 @@
 #include "aiv_driver.h" 
 
-bool node_shutdown  = false;
-void MySigintHandler(int sig);
+bool node_shutdown = false;
 
-int main(int argc, char* argv[])
-{	
+void MySigintHandler(int sig) {
+  node_shutdown = true;
+};
+
+int main(int argc, char* argv[]) {	
 	ros::init(argc, argv, "aiv_driver_node");
-	AIV_Driver my_port;
-	my_port.InitCom("/dev/ttyS1");
-	my_port.CreateThread(ReadDataThread);
-	
-	//my_port.SendCmd(my_port.enable_motor,AIV_Driver::enable_motor_finish);
+	AIV_Driver driver_port;
+	driver_port.InitCom("/dev/ttyS1");
+	driver_port.CreateThread(ReadDataThread);
+
 	//my_port.SendCmd(my_port.req_vel_start,AIV_Driver::req_vel_start_finish);
 
-	//my_port.SendCmd(my_port.req_encoder_start,AIV_Driver::req_encoder_start_finish);
-
-	//my_port.SendCmd(my_port.req_ultra_start,AIV_Driver::req_ultra_start_finish);
-
-	//my_port.SendCmd(my_port.req_bumper_start,AIV_Driver::req_bumper_start_finish);
-
-	if(node_shutdown == true)
-	{
-		my_port.CloseCom("/dev/ttyS1");
+	if(node_shutdown == true) {
+		driver_port.CloseCom("/dev/ttyS1");
 	}
-	
+
 	ros::spin();
 
 	return 0;	
 }
-
-void MySigintHandler(int sig)
-{
-	node_shutdown = true;
-}
-
 
