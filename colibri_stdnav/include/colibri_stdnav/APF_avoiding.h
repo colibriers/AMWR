0d5ccb01032b94e2e3adff7461064750674e1152
shevchenco123 <1007380426@qq.com>
2018-03-17 16:48:44 +0800

    using eigen to refactor the APF module

--- colibri_stdnav/include/colibri_stdnav/APF_avoiding.h
+++ colibri_stdnav/include/colibri_stdnav/APF_avoiding.h
@@ -7,8 +7,11 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <limits.h>
 #include <string>
 #include <vector>
+
+#include <eigen3/Eigen/Core>
 
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
@@ -17,8 +20,6 @@
 
 #include "colibri_msgs/AngPotnEngy.h"
 #include "colibri_msgs/EnvSecurity.h"
-
-using namespace std;
 
 #define RAD2DEG 	180.0/M_PI
 #define DEG2RAD 	M_PI/180.
@@ -29,30 +30,30 @@ using namespace std;
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-#define NUM_RAY4CA 				181		//every degree for front semi-cirle
+#define NUM_RAY4CA 				181			//every degree for front semi-cirle
 #define ANGLE4CA_START 			0
 #define ANGLE4CA_START_INDEX 	61		// 61=(0-(-30))/0.5+1
 #define ANGLE4CA_END 			180
 #define ANGLE4CA_END_INDEX 		421		// 421=(180-(-30))/0.5+1
 #define LASER_BLIND 			0.06
-#define RAY_RESOL4CA			1.0		//laser ray resolution for colision avoidence
+#define LASER_RANGE				20.0
+#define RAY_RESOL4CA			1.0			//laser ray resolution for colision avoidence
 
 #define LASER4CA_FILL_DIS 2.0
 
-#define K_SF 	1.25		//adj factor for latitude dir in polar frame
+#define K_SF 	1.25						//adj factor for latitude dir in polar frame
 #define WIDTH 	0.56
-#define D_SF	0.336	//0.5*K_SF*WIDTH  lateral safe dis
+#define D_SF	K_SF*WIDTH/2.0		//0.5*K_SF*WIDTH  lateral safe dis
 
 #define K_SR 	1.1		//adj factor for longitude dir in polar frame
 #define ACC_DEC -2.0		//accerlaration for decrease vel
+
 #define D_M 	5.0		// local ca distance
-#define KP_PHI_INF 10000.0
+#define KP_PHI_INF 65536.0
 
 #define V_MAX 		0.65
 #define V_MIN 		0.02
-#define THETA_V_MAX 0.45
-
-#define KRF_CORRECTOR 0.1
+#define THETA_V_MAX DEG2RAD_PARAM(150)
 
 #define PASSFCN_THD_RATIO 	0.2
 #define MAX_PASSFCN_SCOPE	0.1
@@ -61,43 +62,41 @@ using namespace std;
 class APF
 {
 	public:
+		typedef Eigen::Array<float, 1, NUM_RAY4CA> Array_CA;
 		
-		float *ptrScan4ca;
-		float scan4ca[NUM_RAY4CA];
-		float ultra4ca[NUM_RAY4CA];
+		Array_CA scan4ca_;
+		Array_CA add_obs4ca_;
 
 		float abstract_pf[NUM_RAY4CA];
+
+		Array_CA delta_phi_vec_;
+		Array_CA kp_phi_vec_;
+
+		Array_CA krf_vec_;
+		Array_CA kaf_vec_;
+		Array_CA passfcn_vec_;
+		
+		Eigen::Array<int, 1, NUM_RAY4CA> phi_start_vec_;
+		Eigen::Array<int, 1, NUM_RAY4CA> phi_end_vec_;
 
 		float min_laser;
 		int min_laser_dir;
 
-		float add_obs4ca[NUM_RAY4CA];
+		float dsr_v_;
 		
-		float delta_phi_vec[NUM_RAY4CA];
-		float kp_phi_vec[NUM_RAY4CA];
+		float max_passfcn_val_;
+		int fwd_maxpass_cnt_ ;
+		int bwd_maxpass_cnt_ ;
+		int maxfcn_fwdbnd_;
+		int maxfcn_bwdbnd_;
 
-		float krf_vec[NUM_RAY4CA];
-		float kaf_vec[NUM_RAY4CA];
-		float passfcn_vec[NUM_RAY4CA];
-		float max_passfcn_val;
-
-		int fwd_maxpass_cnt ;
-		int bwd_maxpass_cnt ;
-
-		int maxfcn_fwdbnd;
-		int maxfcn_bwdbnd;
-
-		int phi_start_vec[NUM_RAY4CA];
-		int phi_end_vec[NUM_RAY4CA];
-
-		float goal_dir;	//goal direction in degree
-		float v;
-		float vth;
+		float goal_dir_;	//goal direction in degree
+		float ctrl_v_;
+		float ctrl_vth_;
 		
-		float angle_adj;
+		float angle_adj_;
 		float vel_adj;
-		unsigned int apf_alarm;
-		unsigned int wander;
+		unsigned int apf_alarm_;
 		
 		ros::NodeHandle nh_ca;
 		ros::Subscriber scan_sub4ca;
@@ -106,33 +105,36 @@ class APF
 
 		colibri_msgs::AngPotnEngy pf_dbg;
 
-		
 		APF();
 		~APF();
 		
-		float CalcKpPhi(float vel_center, float d_phi);
-		void CalcKrfTheta(float* ptrKp_phi_vector, int* ptrPhi_range_start, int* ptrPhi_range_end);	
-		void CalcPhiRange(int i, int range_num, int* ptrPhi_start, int* ptrPhi_end);
-		void CalcPassFcnAndFwdBnd(unsigned int flag, float* max_passfcn_val, float* ptrK_pg);
-		void CalcPassFcnWithoutRPF(float* max_passfcn_val, float* ptrK_pg, float* heading);
-		void CalcPassFcnAndBwdBnd(unsigned int flag, float* max_passfcn_val, float* ptrK_pg);
-		float CalcAdjDir(float* ptrPass_fcn_vector, float max_passfcn_val, int* fwd_bound, int* bwd_bound);
+		void CalcPhiRange(const Eigen::Array<int, 1, NUM_RAY4CA> &range_num);
+		void CalcKpPhi(const float & vel);
+		void CalcPhiParam(const float & velocity);
+		
+		void CalcKafTheta(const float& dir_goal_inlaser);		
+		void CalcKrfTheta(void);
 		void CalcCorrectedKrf(void);
+
+
+		void CalcPassFcn(const int  & seq_flag, const bool & concern_apf);
+		void CalcPassFcnWithoutRPF(void);
+
+		float CalcAdjDir(void);
+
 		void CalcAlarmInAPF(void);
-		void CalcPhiParam(float vel_center, float& dir_goal_inlaser);
 		void ResetMaxPassValCnt(void);
-		void LimitAngle(float& delta_ang);
+
 		void TrimLaserRange4CA(float & compensate);
 		void PubPfInfo4Dbg(void);
-
 			
 	private:
 
 		void ScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_ca);
 		void EnvSecurityCallBack(const colibri_msgs::EnvSecurity::ConstPtr& env);
 
-		float CalcDsrVc(float vel_center);
-		float CalcKrfCorrectFactor(int index);
+		void CalcDsrVc(const float & vel);
+		void LimitAngle(Array_CA & delta_ang);
 
 };
 
--- colibri_stdnav/src/APF_avoiding.cc
+++ colibri_stdnav/src/APF_avoiding.cc
@@ -1,51 +1,31 @@
 #include "APF_avoiding.h"
 
-APF::APF()
-{
-	goal_dir = 60;	//goal direction
-	memset(scan4ca, 0.2, NUM_RAY4CA);
-	ptrScan4ca = scan4ca;
-
-	memset(ultra4ca, 20.0, NUM_RAY4CA);
-	memset(add_obs4ca, 20.0, NUM_RAY4CA);
+APF::APF(float init_scan_dis = 0.2, float init_goal_dir = 90.0){
+				  
 	memset(abstract_pf, 20.0, NUM_RAY4CA);
 	
-
+	goal_dir_ = init_goal_dir;
 	
-	memset(delta_phi_vec, 0, NUM_RAY4CA);
-	memset(kp_phi_vec, 0, NUM_RAY4CA);
-
-	memset(phi_start_vec, 1, NUM_RAY4CA);
-	memset(phi_end_vec, 181, NUM_RAY4CA);
-
-	memset(krf_vec, 1/D_M, NUM_RAY4CA);
-	memset(kaf_vec, 0.0, NUM_RAY4CA);
-
-	memset(passfcn_vec, 0, NUM_RAY4CA);
-	max_passfcn_val = 0.0;
+	max_passfcn_val_ = 0.0;
 
 	min_laser = 20.0;
 	min_laser_dir = 90;
 
-	fwd_maxpass_cnt = 0;
-	bwd_maxpass_cnt = 0;
+	fwd_maxpass_cnt_ = 0;
+	bwd_maxpass_cnt_ = 0;
 
-	maxfcn_fwdbnd = 0;
-	maxfcn_bwdbnd = 0;
+	maxfcn_fwdbnd_ = 90;
+	maxfcn_bwdbnd_ = 90;
 
-	v = 0.0;
-	vth = 0.0;
+	ctrl_v_ = 0.0;
+	ctrl_vth_ = 0.0;
 	
-	angle_adj = 0.0;
+	angle_adj_ = 0.0;
 	vel_adj = 0.0;
-	apf_alarm = 0;
-
-	wander = 0;
+	apf_alarm_ = 0;
 	
 	scan_sub4ca = nh_ca.subscribe<sensor_msgs::LaserScan>("/scan", 1, &scan_ca::ScanCallBack, this);
 	env_sub4safe = nh_ca.subscribe<colibri_msgs::EnvSecurity>("/env_secure", 1, &scan_ca::EnvSecurityCallBack,this);
-	//apf_pub4mntr = nh_ca.advertise<colibri_msgs::AngPotnEngy>("/apf", 5);
-	//rf_pub4mntr = nh_ca.advertise<colibri_msgs::AngPotnEngy>("/rf", 5);
 	pf_Pub4dbg = nh_ca.advertise<colibri_msgs::AngPotnEngy>("/pf_dbg", 5); 
 }
 
@@ -54,28 +34,170 @@ APF::~APF()
 	
 }
 
-void APF::ScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_ca)
-{
-	int j = 61;
-	for(int i = 0; i < NUM_RAY4CA; i++)
-	{
-		scan4ca[i] = scan_ca->ranges[j];
-/*		if(scan4ca[i] < 0.1)
-		{
-			scan4ca[i] = (scan_ca->ranges[j-2]+scan_ca->ranges[j+2]+scan_ca->ranges[j-5]+scan_ca->ranges[j+5]) / 4 + LASER_EDGE_MIN;
-		}
-*/
-		if(scan4ca[i] < 0.05)
-		{
-			scan4ca[i] = scan4ca[i-1];
-		}
-		j = j + 2; 
-
-		add_obs4ca[i] = 20.0;
+void APF::CalcPhiRange(const Eigen::Array<int, 1, NUM_RAY4CA> &range_num) {
+	Eigen::Array<int, 1, NUM_RAY4CA> index_angle;
+	index_angle.setLinSpaced(NUM_RAY4CA, 0, 180);
+	Eigen::Array<int, 1, NUM_RAY4CA> bnd;
 	
+	bnd.setOnes();
+	Eigen::Array<int, 1, NUM_RAY4CA> index_diff = index_angle - range_num;
+	phi_start_vec_ = index_diff.max(bnd);
+
+	bnd = NUM_RAY4CA * bnd;
+  index_diff = index_angle + range_num;
+	phi_end_vec_ = index_diff.min(bnd);
+}
+
+void APF::CalcKpPhi(const float & vel) {
+  CalcDsrVc(vel);
+	Array_CA kp_phi_tmp = (scan4ca_ - dsr_v_).inverse();
+
+	Array_CA val;
+	val = KP_PHI_INF * val.setOnes();
+
+	Array_CA bnd;
+	bnd = dsr_v_ * bnd.setOnes();
+  kp_phi_vec_ = (bnd >= scan4ca_).select(val, kp_phi_tmp);
+	
+	bnd = D_M * bnd.setOnes();
+	val = 1 / (D_M - dsr_v_) * val.setOnes();
+	kp_phi_vec_ = (scan4ca_ >= bnd).select(val, kp_phi_tmp);
+}
+
+void APF::CalcPhiParam(const float & velocity) {
+
+  Eigen::Array<int, 1, NUM_RAY4CA> range_num;
+	
+	delta_phi_vec_ = RAD2DEG * (D_SF * scan4ca_.inverse()).asin();
+	range_num = (delta_phi_vec_ / RAY_RESOL4CA).cast<int>;
+	CalcPhiRange(range_num);	
+
+	CalcKpPhi(velocity);
+
+	krf_vec_ = kp_phi_vec_;
+}
+
+void APF::CalcKafTheta(const float& dir_goal_inlaser) {
+
+	Eigen::Array<float, 1, NUM_RAY4CA> index_angle, tmp_delta;
+	index_angle.setLinSpaced(NUM_RAY4CA, 0., 180.);
+
+	tmp_delta = index_angle - dir_goal_inlaser; 		// unit in degree
+	LimitAngle(tmp_delta);
+	kaf_vec_ = (tmp_delta * DEG2RAD).cos();	//dir_goal_in_laser is 0~180 degree from right side
+}
+
+void APF::CalcKrfTheta(void) {
+	for(int theta_index = 0; theta_index < NUM_RAY4CA; theta_index++) {
+		for(int phi_index = 0; phi_index < NUM_RAY4CA; phi_index++) {
+			if((theta_index >= phi_start_vec_(phi_index)) && (theta_index <= phi_end_vec_(phi_index))) {
+				if(krf_vec_(theta_index) < kp_phi_vec_(phi_index)) {	
+					krf_vec_(theta_index) = kp_phi_vec_(phi_index);	// to obtain the max [Krf(phi,theta)] as Krf(theta)				
+				}
+			}		
+		}
+	}
+}
+
+void APF::CalcCorrectedKrf(const float & krf_corrector) {
+	Eigen::Array<float, 1, NUM_RAY4CA> index_angle, tmp_delta;
+	index_angle.setLinSpaced(NUM_RAY4CA, 0., 180.);
+	
+	Eigen::Array<float, 1, NUM_RAY4CA> puv = (index_angle - 90.0) / 90.;
+
+	krf_vec_ = (1.0 + krf_corrector * puv.pow(2)) * krf_vec_;
+}
+
+void APF::CalcPassFcn(const int  & seq_flag, const bool & concern_apf) {
+	int col = 90;
+	Array_CA tmp_vec;
+	if(concern_apf) {
+		passfcn_vec_ = kaf_vec_ / kaf_vec_;
+		
+	} else {
+		passfcn_vec_ = 1. / kaf_vec_;
+		
+	}
+
+	if(1 == seq_flag) {
+		max_passfcn_val_ = passfcn_vec_.maxCoeff(&col);
+		maxfcn_fwdbnd_ = col;
+		maxfcn_bwdbnd_ = maxfcn_fwdbnd_;
+		
+	} else if(-1 == seq_flag) {
+		tmp_vec = passfcn_vec_.reverse();
+		max_passfcn_val_ = tmp_vec.maxCoeff(&col);
+		maxfcn_bwdbnd_ = ANGLE4CA_END - col;
+		maxfcn_fwdbnd_ = maxfcn_bwdbnd_;
+		
+	} else {
+		max_passfcn_val_ = passfcn_vec_.maxCoeff(&col);
+		maxfcn_fwdbnd_ = col;
+		
+		tmp_vec = passfcn_vec_.reverse();
+		max_passfcn_val_ = tmp_vec.maxCoeff(&col);
+		maxfcn_bwdbnd_ = ANGLE4CA_END - col;
+	}	
+}
+
+void APF::CalcPassFcnWithoutRPF(void) {	
+	int col = 90;
+	passfcn_vec_ = D_M * kaf_vec_;
+	
+	//0 to 180 deg search max passfcn val  and  max passfcn's bound index  locate at right side
+	max_passfcn_val_ = passfcn_vec_.maxCoeff(&col); 
+	angle_adj_ = col - 90.0; 
+}
+
+void APF::CalcAdjDir(void) {
+	int mid_num = 90;
+
+	if(abs(maxfcn_fwdbnd_ - maxfcn_bwdbnd_) < 3)
+	{
+		angle_adj_ =  (maxfcn_fwdbnd_ + maxfcn_bwdbnd_) / 2.0 - mid_num;
+		return;
+	}
+
+	float delta = MIN(PASSVAL_TOLLERENCE * max_passfcn_val_, MAX_PASSFCN_SCOPE);
+
+	//backward directrion(CW 180->0) search record
+	for(int m = 0; m <= mid_num; m++) {
+		if(abs(passfcn_vec_(maxfcn_bwdbnd_ - m) - max_passfcn_val_) < delta) {
+			bwd_maxpass_cnt_++;
+		} else {
+			break;
+		}
 	}
 	
+	// forward direction(CCW 0->180) search record 
+	for(int n = 0; n <= mid_num; n++) {
+		if(abs(passfcn_vec_(maxfcn_bwdbnd_ + n) - max_passfcn_val_) < delta) {
+			fwd_maxpass_cnt_++;
+		} else {
+			break;
+		}
+	}
+
+	if(fwd_maxpass_cnt_ <= bwd_maxpass_cnt_) {
+		angle_adj_ = maxfcn_bwdbnd_ - bwd_maxpass_cnt_ / 2.0 - mid_num;
+	} else {
+		angle_adj_ = maxfcn_bwdbnd_ + fwd_maxpass_cnt_ / 2.0 - mid_num;
+	}
 }
+
+void APF::CalcAlarmInAPF(void) {
+	if(max_passfcn_val_ <= PASSFCN_THD_RATIO * D_M) {
+		apf_alarm_ = 1;
+	} else {
+		apf_alarm_ = 0;
+	}	
+}
+
+void APF::ResetMaxPassValCnt(void) {
+	fwd_maxpass_cnt_ = 0;
+	bwd_maxpass_cnt_ = 0;
+}
+
 void APF::TrimLaserRange4CA(float & compensate)
 {
 	int half_width = 0;
@@ -91,7 +213,7 @@ void APF::TrimLaserRange4CA(float & compensate)
 		{
 			if((i > 0) && (i < NUM_RAY4CA))
 			{
-				add_obs4ca[i] = min_laser;
+				add_obs4ca_[i] = min_laser;
 			}
 			else
 			{
@@ -100,275 +222,9 @@ void APF::TrimLaserRange4CA(float & compensate)
 				
 		}
 	}
-	
-
 }
 
-void APF::EnvSecurityCallBack(const colibri_msgs::EnvSecurity::ConstPtr& env)
-{
-	min_laser = env->laser_min_dis;
-	min_laser_dir = env->laser_min_angle;
-
-}
-
-
-void APF::CalcKrfTheta(float* ptrKp_phi_vector, int* ptrPhi_range_start, int* ptrPhi_range_end)
-{
-
-	for(int theta_index = 0; theta_index < NUM_RAY4CA; theta_index++)
-	{
-		for(int phi_index = 0; phi_index < NUM_RAY4CA; phi_index++)
-		{
-			if((theta_index >= *(ptrPhi_range_start+phi_index)) && (theta_index <= *(ptrPhi_range_end+phi_index)))
-			{
-				if(krf_vec[theta_index] <= *(ptrKp_phi_vector + phi_index)) // to obtain the max [Krf(phi,theta)] as Krf(theta)
-				{	
-					krf_vec[theta_index] = *(ptrKp_phi_vector + phi_index);				
-				}
-
-			}
-
-			
-		}
-	}
-	
-}
-
-void APF::CalcPhiRange(int i, int range_num, int* ptrPhi_start, int* ptrPhi_end)
-{
-	int index_start = 0;
-	int index_end = 0;
-
-	index_start = i - range_num;
-	index_end = i + range_num;
-
-	if(index_start < 1)
-	{
-		*ptrPhi_start = 1;
-		*ptrPhi_end = index_end;
-	}else if(index_end > NUM_RAY4CA)
-	{
-		*ptrPhi_start = index_start;
-		*ptrPhi_end = NUM_RAY4CA;				
-	}else
-	{
-		*ptrPhi_start = index_start;
-		*ptrPhi_end = index_end;
-	}
-
-}
-
-void APF::CalcPassFcnAndFwdBnd(unsigned int flag, float* max_passfcn_val, float* ptrK_pg)
-{
-	float tmp_passfcn_value = 0.0;
-	int tmp_bound_forward = 0;
-		
-		for(int j = 0; j < NUM_RAY4CA; j++) //from right to left search the max passfcn val  and the max passfcn's bound index should locate at left terminal
-		{
-			if(flag == 1)
-			{
-				*(ptrK_pg + j) = 1 / (krf_vec[j]);
-			}
-			else
-			{
-				*(ptrK_pg + j) = kaf_vec[j] / (krf_vec[j]);
-				abstract_pf[j] = 1 / (krf_vec[j]);
-			}
-		
-			if(tmp_passfcn_value <= *(ptrK_pg + j))
-			{
-				tmp_bound_forward = j;
-				tmp_passfcn_value = *(ptrK_pg + j);
-			}
-
-			
-		}
-		
-		maxfcn_fwdbnd = tmp_bound_forward;
-		*max_passfcn_val = tmp_passfcn_value;
-
-}
-
-void APF::CalcPassFcnWithoutRPF(float* max_passfcn_val, float* ptrK_pg, float* heading)
-{	
-	vector<float> tmp_apf;
-	
-	for(int j = 0; j < NUM_RAY4CA; j++) //from right to left search the max passfcn val  and the max passfcn's bound index should locate at left terminal
-	{
-
-		*(ptrK_pg + j) = D_M * kaf_vec[j] ;
-		tmp_apf.push_back(kaf_vec[j]);
-	}
-
-	std::vector<float>::iterator biggest = std::max_element(tmp_apf.begin(), tmp_apf.end());  
-	*heading = float(std::distance(tmp_apf.begin(), biggest)) - 90.0;  
-	 
-	*max_passfcn_val = D_M * (*biggest);
-}
-
-void APF::CalcPassFcnAndBwdBnd(unsigned int flag, float* max_passfcn_val, float* ptrK_pg)
-{
-	float tmp_passfcn_value = 0.0;
-	int tmp_bound_backward = 0;
-
-	for(int k = NUM_RAY4CA - 1; k >= 0; k--) //from left to right search the max passfcn val  and the max passfcn's bound index should locate at right terminal
-	{
-		if(flag == 1)
-		{
-			*(ptrK_pg + k) = 1 / (krf_vec[k]);
-		}
-		else
-		{
-			*(ptrK_pg + k) = kaf_vec[k] / (krf_vec[k]);
-		}		
-		if(tmp_passfcn_value <= *(ptrK_pg + k))
-		{
-			tmp_bound_backward = k;
-			tmp_passfcn_value = *(ptrK_pg + k);
-		}	
-	}
-		
-	maxfcn_bwdbnd = tmp_bound_backward;
-	*max_passfcn_val = tmp_passfcn_value;
-
-}
-
-void APF::CalcAlarmInAPF(void)
-{
-	if(max_passfcn_val <= PASSFCN_THD_RATIO * D_M)
-	{
-		apf_alarm = 1;
-	}
-	else
-	{
-		apf_alarm = 0;
-	}	
-}
-
-float APF::CalcAdjDir(float* ptrPassfcn_vector, float max_passfcn_val, int* fwd_bound, int* bwd_bound)
-{
-	int mid_num = 90;
-	float adj_dir = 0.0;
-
-	float tmp_scope = MIN(PASSVAL_TOLLERENCE * max_passfcn_val, MAX_PASSFCN_SCOPE);
-	
-	for(int m = 0; m <= mid_num; m++)  //backward directrion(CW 180->0) search using forward bound
-	{
-		if(abs(*(ptrPassfcn_vector + *(fwd_bound) - m) - max_passfcn_val) < tmp_scope)
-		{
-			fwd_maxpass_cnt++;
-		}else
-		{
-			break;
-		}
-	}
-	for(int n = 0; n <= mid_num; n++)	// forward direction(CCW 0->180) search using backward bound
-	{
-		if(abs(*(ptrPassfcn_vector + *(bwd_bound) + n) - max_passfcn_val) < tmp_scope)
-		{
-			bwd_maxpass_cnt++;
-		}else
-		{
-			break;
-		}
-	}
-
-	
-	if (fwd_maxpass_cnt <= bwd_maxpass_cnt)
-	{
-		adj_dir = *bwd_bound + bwd_maxpass_cnt / 2.0 - mid_num;
-	}else
-	{
-		adj_dir = *fwd_bound - fwd_maxpass_cnt / 2.0 - mid_num;
-	}
-
-	return adj_dir;
-
-}
-		
-float APF::CalcDsrVc(float vel_center)
-{
-	float d_sr_v = 0.0;
-	
-	d_sr_v = -0.5 * K_SR * vel_center * vel_center / ACC_DEC;
-
-	return d_sr_v;
-}
-
-float APF::CalcKpPhi(float vel_center, float d_phi)
-{
-	float tmpDsr = 0.0;
-	float kp_phi = 0.0;
-	tmpDsr = CalcDsrVc(vel_center);
-
-	if(d_phi <= tmpDsr)
-	{
-		kp_phi = KP_PHI_INF;
-	}
-	else if((d_phi <= D_M)&&(tmpDsr < d_phi))
-	{
-		kp_phi = 1 / (d_phi - tmpDsr);
-	}
-	else if(d_phi > D_M)
-	{
-		kp_phi = 1 / (D_M - tmpDsr);
-	}
-	else
-	{
-		cout<<"Calc Kp_phi exception in CalcKpPhi Function ! : " <<endl;
-		cout<< "tmpDsr : "<< tmpDsr <<endl;
-		cout<< "d_phi : "<< d_phi <<endl;
-	}
-	
-	return kp_phi;
-}
-
-void APF::ResetMaxPassValCnt(void)
-{
-	fwd_maxpass_cnt = 0;
-	bwd_maxpass_cnt = 0;
-}
-
-void scan_ca::LimitAngle(float & delta_ang)
-{
-	if(delta_ang >= 90) // for calc attract field , the delta angle should be -90~90 deg to ignore neg val
-	{
-		delta_ang = 90;
-	}
-	else if (delta_ang <= -90)
-	{
-		delta_ang = -90;
-	}
-	else
-	{
-
-	}
-}
-
-void APF::CalcCorrectedKrf(void)
-{
-	float corrector;
-	
-	for(int i = 0; i < NUM_RAY4CA; i++)
-	{
-		corrector = CalcKrfCorrectFactor(i);
-		krf_vec[i] = corrector * krf_vec[i];
-	}	
-}
-
-float APF::CalcKrfCorrectFactor(int index)
-{
-	//to make the scan front circle krf_vec factor as : y = 1+k * pow(x,2)
-	float factor = 1.0;
-	float tmp_puv = (90 - index) / 90.0; 	// index : 0~180
-	
-	factor = 1.0 + KRF_CORRECTOR * pow(tmp_puv, 2);
-
-	return factor;
-}
-
-void APF::PubPfInfo4Dbg(void)
-{
+void APF::PubPfInfo4Dbg(void) {
 	pf_dbg.header.stamp = ros::Time::now();
 	pf_dbg.header.frame_id = "apf";
 	pf_dbg.angle_min = 0.0;
@@ -376,68 +232,38 @@ void APF::PubPfInfo4Dbg(void)
 	pf_dbg.angle_increment = 1.0;
 
 	memcpy(&pf_dbg.potential_value[0], &abstract_pf[0], sizeof(abstract_pf));
-	pf_Pub4dbg.publish(pf_dbg);
-			
-
+	pf_Pub4dbg.publish(pf_dbg);	
 }
-void APF::CalcPhiParam(float vel_center, float& dir_goal_inlaser)
-{
 
-	int range_num = 0;
-	float tmp_delta = 0.0;
-	float min_multi_range = 20.0;
+void APF::ScanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_ca) {
+	int j = ANGLE4CA_START_INDEX;
+	for(int i = 0; i < NUM_RAY4CA; i++) {
+		scan4ca_(i) = scan_ca->ranges[j];
 
-#ifdef ULTRA_RF	
-	float min_ultra_dis = 3.0;
-	int ultra_obs_coder = 0;
-	int ultra_strategy = 0;
-
-	
-#endif
-
-#ifdef EXT_LASER_RF
-	float compensate = 10.0;
-	TrimLaserRange4CA(compensate);
-#endif
-
-	for(int i = 0; i < NUM_RAY4CA; i++)
-	{
-
-#ifdef ORI_ULTRA_FUSION
-		min_multi_range = MIN(*(ptrScan4ca + i), ultra4ca[i]);
-		delta_phi_vec[i] = asin(D_SF / min_multi_range) * RAD2DEG; //calc the phi ang obs influence range
-#endif
-
-#ifdef ORI_EXTLASER_FUSION
-		min_multi_range = MIN(*(ptrScan4ca + i), add_obs4ca[i]);
-		delta_phi_vec[i] = asin(D_SF / min_multi_range) * RAD2DEG; //calc the phi ang obs influence range
-#endif
-
-#ifdef ORI_EXT_ULTRA_FUSION
-		min_multi_range = MIN(*(ptrScan4ca + i), add_obs4ca[i]);
-		min_multi_range = MIN(min_multi_range, ultra4ca[i]);
-		delta_phi_vec[i] = asin(D_SF / min_multi_range) * RAD2DEG; //calc the phi ang obs influence range
-#endif
-
-
-#ifdef NO_FUSION
-		delta_phi_vec[i] = asin(D_SF / (*(ptrScan4ca + i))) * RAD2DEG;
-#endif
-
-		range_num = floor(delta_phi_vec[i] / RAY_RESOL4CA);
-		CalcPhiRange(i, range_num, &phi_start_vec[i], &phi_end_vec[i]);	
-
-		kp_phi_vec[i] = CalcKpPhi(vel_center, *(ptrScan4ca + i));		
-		krf_vec[i] = kp_phi_vec[i];
-	
-		tmp_delta = i - dir_goal_inlaser;			// unit in degree
-		LimitAngle(tmp_delta);
-		kaf_vec[i] = cos(tmp_delta * DEG2RAD);  //dir_goal_in_laser is 0~180 degree from right side
-		
+		if(scan4ca_(i) < LASER_BLIND) {
+			scan4ca_(i) = scan4ca_[i-1];
+		}
+		j = j + 2; 
 	}
-
+	
+	Eigen::Array<float, 1, NUM_RAY4CA> tmp;
+	add_obs4ca_.swap(LASER_RANGE* tmp.setOnes());
+	
 }
 
+void APF::EnvSecurityCallBack(const colibri_msgs::EnvSecurity::ConstPtr& env) {
+	min_laser = env->laser_min_dis;
+	min_laser_dir = env->laser_min_angle;
+}
 
+void APF::CalcDsrVc(const float & vel) {
+	dsr_v_ = -0.5 * K_SR * vel * vel / ACC_DEC;
+}
 
+void APF::LimitAngle(Array_CA & delta_ang) {
+	Array_CA right_ang;
+	right_ang = 90. * right_ang.setOnes();	
+	delta_ang = delta_ang.min(right_ang);
+	delta_ang = delta_ang.max(-right_ang);
+}
 
